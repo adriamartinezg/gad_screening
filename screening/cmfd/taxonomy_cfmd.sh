eab39cab9492bafe657e9b5c4c48d9b4c6940890
adriamartinezg <adriamartinezg@gmail.com>
2026-07-06 23:18:00 +0200

    Add cfmd screening

---
+++ screening/cmfd/taxonomy_cfmd.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+mkdir -p results_cfmd/taxonomy
+
+CFMD_LIST="results_cfmd/taxonomy/cFMD_mags_list.tsv"
+if [ ! -f "$CFMD_LIST" ]; then
+    echo "Descargando lista de taxonomía de cFMD..."
+    wget "https://raw.githubusercontent.com/SegataLab/cFMD/refs/heads/main/cFMD_mags_list.tsv" \
+     -O "$CFMD_LIST"
+fi
+
+# 1. Extraer ID, Categoría y STATUS desde tu screening de GAD
+# Asumiendo que $3 es ID, $5 es Categoría y $9 es System_Status
+awk -F'\t' '$9 == "COMPLETE" || $9 == "ABSENT" {print $3 "\t" $5 "\t" $9}' GAD_SYSTEM_MAG_LEVEL_RESULTS.tsv > mag_ids_all.tmp
+
+OUTPUT="results_cfmd/taxonomy/final_taxonomy_results_all.tsv"
+
+# 2. Cabecera EXACTA para que R no se pierda
+echo -e "MAG_id\tCategory\tSystem_Status\tSpecies\tKuperkingdom\tPhylum\tClass\tOrder\tFamily\tGenus\tCompleteness\tContamination" > "$OUTPUT"
+
+# 3. Bucle de asignación de taxonomía
+echo "Asignando taxonomía a los MAGs..."
+while read -r mag_id category status; do
+    line=$(grep -w "$mag_id" "$CFMD_LIST")
+    if [ -n "$line" ]; then
+        specie=$(echo "$line" | cut -f14)
+        skingdom=$(echo "$line" | cut -f8)
+        phylum=$(echo "$line" | cut -f9)
+        class=$(echo "$line" | cut -f10)
+        order=$(echo "$line" | cut -f11)
+        family=$(echo "$line" | cut -f12)
+        genus=$(echo "$line" | cut -f13)
+        completeness=$(echo "$line" | cut -f18)
+        contamination=$( echo "$line" | cut -f19)
+
+        echo -e "$mag_id\t$category\t$status\t$specie\t$skingdom\t$phylum\t$class\t$order\t$family\t$genus\t$completeness\t$contamination" >> "$OUTPUT"
+    fi
+done < mag_ids_all.tmp
+
+# Limpieza
+[ -f mag_ids_all.tmp ] && rm mag_ids_all.tmp
+
+echo "¡Proceso finalizado con éxito!"
+echo "Archivos generados en results_cfmd/taxonomy/"
+
